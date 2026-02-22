40548966dfda7fc5544e262157467849d39317bb
Godbless Yankson <godblessyankson06@gmail.com>
2026-02-22 21:13:56 +0000

    Data Financa

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './index.css'
+const App = () => {
+  return (
+    <div>
+      <p className='text-blue-500 text-3xl bg-red-400'>I am about to write a new code</p>
+    </div>
+  )
+}
+
+export default App
